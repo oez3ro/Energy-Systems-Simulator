8e6b1af5e5587c20cc018d5906095fc8eaba1198
oez3ro <oe.z3ro@gmail.com>
2026-04-11 11:01:03 -0500

    Delete OpenCV directory

--- OpenCV/L1_HW/H@_HSV.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img = cv2.imread('landscape.png', 1)
-
-hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-cv2.imshow('img_hsv', hsv_img)
-cv2.imwrite('lndscpsv.png', hsv_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
